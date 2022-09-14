7b868e47c060909cc4a55ee01a846eff91a92f46
Claudia Mathenge <claudia.wabera@gmail.com>
2022-09-14 11:26:41 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - function checks for alphaabetic character
+ * @c: char type letter
+ * Return: return 0 on success and 1 on fail
+ */
+
+int _is alpha(int c)
+{
+	int i = isalpha(c)
+
+		if (i > 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
